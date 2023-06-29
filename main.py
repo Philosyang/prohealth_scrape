640a5ac48e319614fc2c7456aef35b10c2d8bd6a
Philos <i@philosyang.com>
2023-06-29 01:43:19 -0500

    > out.csv

--- main.py
+++ main.py
@@ -1,80 +1,96 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import time
 
-page = urllib.request.urlopen(
-    'https://www.prohealth.com/collections/all?page=1')   # TODO: request all 4 urls
-pr = page.read()
+# empty out.csv before running
+with open('out.csv', mode='a', encoding="utf-8") as file:
+    file.write('name, link, price, rating, review_count')
+    file.write('\n')
 
-soup = BeautifulSoup(pr, 'html.parser')
-sp = soup.prettify()
+for i in range(1, 5):
 
-path_w = 'temp.html'
-with open(path_w, mode='w', encoding="utf-8") as f:
-    f.write(sp)
+    page = urllib.request.urlopen(
+        'https://www.prohealth.com/collections/all?page=' + str(i))   # TODO: request all 4 urls
+    pr = page.read()
 
-# temp_formatted.html:
+    soup = BeautifulSoup(pr, 'html.parser')
+    sp = soup.prettify()
 
-# name & href
-# <div class="product-grid--title" data-block-type="product_card_title">
-base_url = 'https://www.prohealth.com'  # needed for proper product href
+    path_w = 'temp.html'
+    with open(path_w, mode='w', encoding="utf-8") as f:
+        f.write(sp)
 
-# price
-# <span class="money">
+    # temp_formatted.html:
 
-# star count
-# <div class="jdgm-prev-badge" data-average-rating="
+    # name & href
+    # <div class="product-grid--title" data-block-type="product_card_title">
+    base_url = 'https://www.prohealth.com'  # needed for proper product href
 
-# review count
-# <span class="jdgm-prev-badge__text">
+    # price
+    # <span class="money">
 
-# product name, product link, price, star count, review count
+    # star count
+    # <div class="jdgm-prev-badge" data-average-rating="
 
-# print(soup.body)    # correct
-# <div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">
+    # review count
+    # <span class="jdgm-prev-badge__text">
 
-# cut the page
-# print(sp)   # good
-# print(type(sp)) # <class 'str'>
+    # product name, product link, price, star count, review count
 
-# print(sp[sp.index('<div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">'): sp.index('<div class="footer-wrapper">')])    # good, = temp_formatted_v2.html
-items = sp[sp.index('<div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">')
-                    :sp.index('<div class="footer-wrapper">')]
+    # print(soup.body)    # correct
+    # <div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">
 
-# print(items[items.index('<div class="grid-area--collection"'):])  # good, = temp_formatted_v3.html
-collection = items[items.index(
-    '<div class="product-grid--title" data-block-type="product_card_title">'):]
+    # cut the page
+    # print(sp)   # good
+    # print(type(sp)) # <class 'str'>
 
-clist = collection.split(
-    '<div class="product-grid--title" data-block-type="product_card_title">')
-# print(clist[0])
-# print(clist[1])
-# print(clist[-1])
+    # print(sp[sp.index('<div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">'): sp.index('<div class="footer-wrapper">')])    # good, = temp_formatted_v2.html
+    items = sp[sp.index('<div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">')               :sp.index('<div class="footer-wrapper">')]
 
-out_list = []
-# print(clist)
-for item in clist:
-    if len(item) < 2:
-        continue
+    # print(items[items.index('<div class="grid-area--collection"'):])  # good, = temp_formatted_v3.html
+    collection = items[items.index(
+        '<div class="product-grid--title" data-block-type="product_card_title">'):]
 
-    ci = item.replace('\n', '')
-    # print(ci)
-    latter_url = ci[ci.index('/collections'):ci.index('">')]
+    clist = collection.split(
+        '<div class="product-grid--title" data-block-type="product_card_title">')
+    # print(clist[0])
+    # print(clist[1])
+    # print(clist[-1])
 
-    item_name = ci[ci.index('">')+len('">'):ci.index('</a>')].strip()
+    out_list = []
+    # print(clist)
+    for item in clist:
+        if len(item) < 2:
+            continue
 
-    item_rating = float(ci[ci.index('data-average-rating="') +
-                        len('data-average-rating="'):ci.index('" data-number-of-questions')])
+        ci = item.replace('\n', '')
+        # print(ci)
+        latter_url = ci[ci.index('/collections'):ci.index('">')]
 
-    item_review_count = int(ci[ci.index('data-number-of-reviews="') +
-                               len('data-number-of-reviews="'):ci.index('style="display:none"')].strip()[:-1])
+        item_name = ci[ci.index('">')+len('">'):ci.index('</a>')].strip()
 
-    ipt = ci[ci.index('<span class="money">') +
-             len('<span class="money">'):ci.index('<div class="unit-price hide"')].strip()
-    # $58.00    </span>
-    item_price = float(ipt[ipt.index('$') + len('$'):ipt.index('</span>')].strip().replace(',', ''))
+        item_rating = float(ci[ci.index('data-average-rating="') +
+                            len('data-average-rating="'):ci.index('" data-number-of-questions')])
 
-    # name, link, price, rating, review_count
-    out_list.append([item_name, base_url+latter_url,
-                    item_price, item_rating, item_review_count])
+        item_review_count = int(ci[ci.index('data-number-of-reviews="') +
+                                len('data-number-of-reviews="'):ci.index('style="display:none"')].strip()[:-1])
 
-print(out_list)
+        ipt = ci[ci.index('<span class="money">') +
+                 len('<span class="money">'):ci.index('<div class="unit-price hide"')].strip()
+        # $58.00    </span>
+        item_price = float(ipt[ipt.index('$') + len('$')
+                           :ipt.index('</span>')].strip().replace(',', ''))
+
+        # name, link, price, rating, review_count
+        out_list.append([item_name, base_url+latter_url,
+                        item_price, item_rating, item_review_count])
+
+    # print(out_list)
+    for item in out_list:
+        with open('out.csv', mode='a', encoding="utf-8") as file:
+            file.write(str(item)[1:-1].replace('\'', ''))
+            file.write('\n')
+
+    if i < 4:
+        print('sleep 5 seconds before next scrape...')
+        time.sleep(5)
