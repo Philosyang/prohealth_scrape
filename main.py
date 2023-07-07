73015c982a303e89e2995ca3b1b1742181299370
Philos <i@philosyang.com>
2023-07-07 16:50:57 -0500

    add categories

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import urllib.request
 import time
 
 with open('out.csv', mode='w', encoding="utf-8") as file:
-    file.write('name, link, price, rating, review_count')
+    file.write(
+        'name, link, price, rating, review_count, Anti-Inflammatory Support, Antioxidant Health, Blood Sugar Support, Body, Bone Health, Brain Health, Cellular Health, DNA Health, Detoxification, Fertility Support, Gut Health, Healthy Aging, Healthy Metabolism, Heart Health, Improved Energy, Joint Health, Liver Support, Mood Support, Muscle Health, Skin Health, Soul, Stress Management'
+    )
     file.write('\n')
 
 page_count = 4
@@ -46,7 +48,8 @@ for i in range(1, page_count+1):
     # print(type(sp)) # <class 'str'>
 
     # print(sp[sp.index('<div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">'): sp.index('<div class="footer-wrapper">')])    # good, = temp_formatted_v2.html
-    items = sp[sp.index('<div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">')               :sp.index('<div class="footer-wrapper">')]
+    items = sp[sp.index('<div data-cart-action="page" data-editor-open="false" data-language-url="/" id="PageContainer">')
+                        :sp.index('<div class="footer-wrapper">')]
 
     # print(items[items.index('<div class="grid-area--collection"'):])  # good, = temp_formatted_v3.html
     collection = items[items.index(
@@ -68,7 +71,8 @@ for i in range(1, page_count+1):
         # print(ci)
         latter_url = ci[ci.index('/collections'):ci.index('">')]
 
-        item_name = ci[ci.index('">')+len('">'):ci.index('</a>')].strip().replace(',','')
+        item_name = ci[ci.index('">')+len('">')
+                                :ci.index('</a>')].strip().replace(',', '')
 
         item_rating = float(ci[ci.index('data-average-rating="') +
                             len('data-average-rating="'):ci.index('" data-number-of-questions')])
@@ -79,8 +83,7 @@ for i in range(1, page_count+1):
         ipt = ci[ci.index('<span class="money">') +
                  len('<span class="money">'):ci.index('<div class="unit-price hide"')].strip()
         # $58.00    </span>
-        item_price = float(ipt[ipt.index('$') + len('$')
-                           :ipt.index('</span>')].strip().replace(',', ''))
+        item_price = float(ipt[ipt.index('$') + len('$'):ipt.index('</span>')].strip().replace(',', ''))
 
         # name, link, price, rating, review_count
         out_list.append([item_name, base_url+latter_url,
@@ -90,6 +93,7 @@ for i in range(1, page_count+1):
     for item in out_list:
         with open('out.csv', mode='a', encoding="utf-8") as file:
             file.write(str(item)[1:-1].replace('\'', ''))
+            file.write(', 0' * 22)
             file.write('\n')
 
     if i < page_count:
